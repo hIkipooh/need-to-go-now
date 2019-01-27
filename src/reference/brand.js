a4612789abc98d6edab053032ab41bc2a464a18c
Brian Lee <40387476+hIkipooh@users.noreply.github.com>
2019-01-27 21:40:14 +0900

    Working on Server

---
+++ src/reference/brand.js
@@ -0,0 +1,3 @@
+module.exports = [
+    {seveneleven : "セブン-イレブン"}
+]
--- src/server.js
+++ src/server.js
@@ -1,1 +1,50 @@
 const express = require('express');
+const config = require('../config');
+const initDB = require('./db/initialDB')
+const Knex = require('knex');
+const knex = Knex({
+    client: config.db.client,
+    port: config.db.port,
+    connection: {
+        host: config.db.connection.host,
+        database: config.db.connection.database
+    }
+})
+
+const server = () => {
+    const app = express();
+    const allConvini = knex('convini').select();
+
+    app.get("/api/convini", (req, res, next) => {
+        const { limit } = req.query;
+        let store = [];
+        if(!limit)return next();
+        allConvini.then(
+            convinis => {
+                console.log('============convinis==========', convinis)
+                for(let i = 1; i < limit + 1; i++) {
+                    each = convinis[i]
+                    store.push(each)
+                }
+                return res.json(store)
+            }
+        )
+        // return res.json(store)
+
+    })
+    app.get("/api/convini", (req, res) => {
+        return allConvini.then(
+            convinis => {
+                convinis.forEach(c => {
+                    res.json(c)
+                })
+            })
+    });
+
+    app.get("/api/")
+
+
+    return app;
+};
+
+module.exports = { server };
