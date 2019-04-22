c63e2aacf0ea85e4cc339d2892ee583c55e701b1
Brian Lee <40387476+hIkipooh@users.noreply.github.com>
2019-04-22 17:06:54 +0900

    access information from the tables from each convini url

--- src/webscrape/webscraping.py
+++ src/webscrape/webscraping.py
@@ -14,6 +14,10 @@ page_soup = soup(page_html, "html.parser")
 # grab each convinience store
 containers = page_soup.findAll("div", {"class": "pickupListText"})
 
-for container in containers
-linkContainer = container.findAll("a", {"target": "_blank"})
-eachConviniUrl = linkConainer[0]["href"]
+for container in containers:
+    linkContainer = container.findAll("a", {"target": "_blank"})
+    eachConviniUrl = linkContainer[0]["href"]
+    eachClient = uReq(eachConviniUrl)
+    page_html2 = eachClient.read()
+    page_soup2 = soup(page_html2, "html.parser")
+    conviniInfoTable = page_soup2.find_all("table", {"class": "table_data"})
