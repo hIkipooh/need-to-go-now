fe788d89ff94cbf347a5b899f15fab45cce73f46
Brian Lee <40387476+hIkipooh@users.noreply.github.com>
2019-04-25 23:11:02 +0900

    delete legacy code

--- src/db/knexfile.js
+++ src/db/knexfile.js
@@ -1,52 +1,39 @@
 module.exports = {
-    development: {
-        client: 'pg',
-        connection:'postgres://localhost/needtogonow',
-        migrations: {
-            directory: './migrations'
-        },
-        seeds: {
-            directory: './seeds'
-        },
-        useNullAsDefault: true
+  development: {
+    client: "pg",
+    connection: "postgres://localhost/needtogonow",
+    migrations: {
+      directory: "./migrations"
     },
+    seeds: {
+      directory: "./seeds"
+    },
+    useNullAsDefault: true
+  },
 
-    test: {
-        client: 'pg',
-        connection: {
-              host: "127.0.0.1",
-              database: "needtogonow",
-            },
-        migrations: {
-            directory: './migrations'
-        },
-        seeds: {
-            directory: './seeds/test'
-        },
-        useNullAsDefault: true
+  test: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      database: "needtogonow"
     },
-    production: {
-        client: 'pg',
-        connection: process.env.DATABASE_URL,
-        migrations: {
-            directory: './migrations'
-        },
-        seeds: {
-            directory: './seeds'
-        },
-        useNullAsDefault: true
-    }
-    // client: "pg",
-    // connection: {
-    //   host: "127.0.0.1",
-    //   database: "needtogonow",
-    // },
-    // port: 5432,
-    // pool: {
-    //   min: 2,
-    //   max: 10,
-    // },
-    // migrations: {
-    //   tableName: "knex_migrations",
-    // },
-  };
+    migrations: {
+      directory: "./migrations"
+    },
+    seeds: {
+      directory: "./seeds/test"
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./migrations"
+    },
+    seeds: {
+      directory: "./seeds"
+    },
+    useNullAsDefault: true
+  }
+};
